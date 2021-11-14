e5034687e7a132a7a6a6fab70c3bca0ff8d2448d
Gabriel Carvalho <gabriel.braga@prevision.com.br>
2021-11-14 16:33:20 -0300

    Componetized header and initialized it

---
+++ src/components/Header/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  v-app-bar(app color="rgb(255, 255, 255, 0.8)")
+    v-container.d-flex.align-center(max-width="1440px")
+      v-img(:src="require('../../assets/SMR.png')" max-width="67px")
+      v-spacer
+      v-avatar(color="grey lighten-2" size="42")
+</template>
+
+<script>
+export default {};
+</script>
