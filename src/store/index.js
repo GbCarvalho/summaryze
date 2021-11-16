22118457f9af8be119e6b3786f5fa8605d0b98b3
Gabriel Carvalho <gabriel.braga@prevision.com.br>
2021-11-16 13:08:11 -0300

    Configuring vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,40 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// import VuexPersistence from "vuex-persist";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+let store = null;
+
+/* TOFIX - This plugin should automatically send all the VueX State to localStorage
+ * but its not permitting somehow the page to load */
+// const vuexLocalStorage = new VuexPersistence({
+//   key: "summaryze",
+//   storage: "localStorage",
+//   reducer: (state) => ({
+//     video: {
+//       cards: state.video.cards,
+//     },
+//   }),
+// });
+
+store = new Vuex.Store({
+  // plugins: [vuexLocalStorage.plugin],
+  state: {
+    video: {
+      cards: [],
+    },
+  },
+  mutations: {
+    addVideo(state, payload) {
+      state.video.cards.push(payload);
+    },
+  },
+  getters: {
+    videos: (state) => state.video.cards,
+  },
   actions: {},
   modules: {},
 });
+
+export default store;
